bafe31a3030800d2b88449d273cd039a385fbe7b
Ufuueueuru <theultimatewiseguy@gmail.com>
2018-02-13 16:52:38 -0700

    Create Controls.js

---
+++ Controls.js
@@ -0,0 +1,10 @@
+function Controls(up, left, down, right, attack, special, shield, pause) {
+	this.up = up;
+	this.left = left;
+	this.down = down;
+	this.right = right;
+	this.attack = attack;
+	this.special = special;
+	this.shield = shield;
+	this.pause = pause;
+}
