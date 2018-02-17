6e97fa2af78b3ba677a153b5a23881c5e5b24f93
Ufuueueuru <theultimatewiseguy@gmail.com>
2018-02-16 19:53:15 -0700

    Update Vars.js

--- Vars.js
+++ Vars.js
@@ -1,12 +1,43 @@
+//Animations
+var quote;
+var flowey;
+var glitch;
+var sans;
+var ballos;
+var fawful;
+//Animations
+//Animation images
+var quote0;
+var quote1;
+var quote3;
+var quote4;
+var quote5;
+var quote7;
+
+var sans0;
+var sans1;
+var sans2;
+var sans3;
+
+var fawful0;
+var fawful1;
+var fawful2;
+var fawful3;
+var fawful4;
+var fawful5;
+var fawful6;
+var fawful7;
+//Animation images
 var playersDown = 0;//Finds the amount of players not in the match
 var winner = 0;//Who won the last match
 var trans = {x: 0, y: 0, scale: 1, count: 0};
 var playerNum = 1;
 var keySelect = [false,0];
 var menu = 3;//0: main menu, 1: settings, 2: character select, 3: intro, 4: actual battle
-var arena = "classic";//what arena you are in
+var arena = new Arena("Classic");//what arena you are in
 var keys = [];
 var fire = [];
+
 var title;//images
 var quotePic;
 var floweyPic;
@@ -14,6 +45,8 @@ var glitchPic;
 var sansPic;
 var balosPic;
 var fawfulPic;//images
+
+var ai = new AI();
 var slideX = [0,0];
 var slideY = [0,0];
 var loading = 0;
@@ -39,10 +72,16 @@ var choose = {
   }
 }
 var controls = [
-	new Controls(38, 37, 40, 39, 90, 88, 67, 32),
+	new Controls(38, 37, 40, 39, 190, 188, 77, 32),
 	new Controls(87, 65, 83, 68, 70, 71, 72, 32),
 	new Controls(254, 254, 254, 254, 254, 254, 254, 254),
 	new Controls(254, 254, 254, 254, 254, 254, 254, 254),
+];
+var cpuControls = [
+	new Controls(false, false, false, false, false, false, false, false),
+	new Controls(false, false, false, false, false, false, false, false),
+	new Controls(false, false, false, false, false, false, false, false),
+	new Controls(false, false, false, false, false, false, false, false)
 ];
 function keyButton(x,y,inp){
   fill(70,70,70);//one button
@@ -77,8 +116,8 @@ var attack = [
   }
 ];
 var player = [
-	new Player(0, 0, false, 0, 0, 0, 0, 1, false, 0, 3, true, new Character(undefined)),
-	new Player(0, 0, false, 0, 0, 0, 0, 1, false, 0, 3, true, new Character(undefined)),
-	new Player(0, 0, true, 0, 0, 0, 0, 1, false, 0, 3, true, new Character(undefined)),
-	new Player(0, 0, true, 0, 0, 0, 0, 1, false, 0, 3, true, new Character(undefined))
+	new Player(0, 0, false, 0, 0, 0, 0, 1, false, 0, 3, true, true, new Character(undefined)),
+	new Player(0, 0, false, 0, 0, 0, 0, 1, false, 0, 3, true, true, new Character(undefined)),
+	new Player(0, 0, true, 0, 0, 0, 0, 1, false, 0, 3, true, true, new Character(undefined)),
+	new Player(0, 0, true, 0, 0, 0, 0, 1, false, 0, 3, true, true, new Character(undefined))
 ];
