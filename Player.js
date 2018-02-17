559ebd233090f5d2bef5f6ab39c59ee84d164a75
Ufuueueuru <theultimatewiseguy@gmail.com>
2018-02-16 19:48:18 -0700

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,17 +1,21 @@
-function Player(x, y, cpu, damage, xVel, yVel, look, dir, attacking, inv, lives, alive, character) {
-  this.x = x;
-  this.y = y;
-  this.cpu = cpu;
-  this.damage = damage;
-  this.xVel = xVel;
-  this.yVel = yVel;
+function Player(x, y, cpu, damage, xVel, yVel, look, dir, attacking, inv, lives, alive, airSpecial, character) {
+	this.x = x;//x value
+  this.y = y;//y value
+	this.shield = false;//If your shield is out
+	this.ground = false;
+	this.shieldNum = 200;//How much shield you have left
+  this.cpu = cpu;//If you are a cpu
+  this.damage = damage;//How much damage you have taken
+  this.xVel = xVel;//x velocity
+  this.yVel = yVel;//y velocity
   this.look = look;//0: neither 1: up 2: down
   this.dir = dir;//1: right 2: left
-  this.attacking = attacking;
-  this.inv = inv;
-  this.lives = lives;
-  this.alive = alive;
-  this.character = character;
+  this.attacking = attacking;//If you are attacking
+  this.inv = inv;//How much invinsibility you have
+	this.lives = lives;//How many lives you have
+	this.alive = alive;//If you are alive
+	this.airSpecial = airSpecial;//Stops you from using special attack twice in the air
+  this.character = character;//The character object
 	
 	this.name = function() {
 		if(this.character.pic === quotePic) {
